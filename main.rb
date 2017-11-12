554dc31bdadb121644f6faf6e5b6a04de835c094
elisaado <elimalkosaado@gmail.com>
2017-11-12 21:51:10 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,63 @@
+require 'sinatra'
+require "sinatra/reloader"
+
+set :strict_paths, false
+
+def guideparser(path)
+  puts path
+  name = "#{path}" # hack
+  name[0..6] = ''
+
+  puts "Parsing: #{name}, #{path}"
+  
+  content = File.read(path)
+  content = content.split("\n")
+  
+  title = content[1] if content[0] == "#t"
+  steps_raw = content[4..-1] if content[3] == "#s"
+  steps = []
+  
+  puts steps_raw.inspect
+
+  steps_raw.each do |step|
+    steps.push step and next if step[0] == " " # hack 
+    steps.push step.gsub(/(\d+)\. /, '')
+  end
+  
+  puts steps.inspect
+  return {title: title, steps: steps, name: name, path: path}
+end
+
+guides = []
+
+puts "Loading stepfiles..."
+Dir.foreach('guides/') do |filename|
+  next if filename == '.' || filename == '..'
+
+  puts "Loading: #{filename}"
+  
+  guides.push guideparser("guides/#{filename}")
+end
+
+get '/' do
+  erb :index
+end
+
+get '/guides' do
+  @guides = guides
+  erb :guide_list
+end
+
+get '/guides/:guide' do
+  guide = nil
+
+  guides.each do |guide_obj|
+    puts guide_obj[:name]
+    guide = guide_obj if params[:guide] == guide_obj[:name]
+  end
+  
+  return "Guide not found!" if guide == nil
+  
+  @guide = guide
+  erb :guide
+end
